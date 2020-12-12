231af9946160f5c6155c0a7193e81254099a3c24
samuelgomes <samuelluz.g@gmail.com>
2020-12-12 10:06:05 -0300

    Refactoring and creating hotel class

---
+++ resources/hotel.py
@@ -0,0 +1,43 @@
+from flask_restful import Resource
+
+hoteis = [
+    {
+    'hotel_id':'alpha',
+    'nome':'Alpha Hotel',
+    'estrelas':4.3,
+    'diaria':420.34,
+    'cidade':'Rio de Janeiro'
+    },
+    {
+    'hotel_id':'bravo',
+    'nome':'Bravo Hotel',
+    'estrelas':4.4,
+    'diaria':380.90,
+    'cidade':'Santa Catarina'
+    },
+    {
+    'hotel_id':'charlie',
+    'nome':'Charlie Hotel',
+    'estrelas':3.9,
+    'diaria':320.20,
+    'cidade':'Santa Catarina'
+    }
+]
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis}
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id']==hotel_id:
+                return hotel
+        return {'message':'Hotel not found.'}, 404 #not found
+    def post(self, hotel_id):
+        pass
+
+    def put(self, hotel_id):
+        pass
+
+    def delete(self, hotel_id):
+        pass
