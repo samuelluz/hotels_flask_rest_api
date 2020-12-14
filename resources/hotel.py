d8982fccdbaa1a39e33ceed2c8f7e68ee8d2e718
samuelgomes <samuelluz.g@gmail.com>
2020-12-14 10:56:39 -0300

    creating hotel model

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -23,6 +24,7 @@ hoteis = [
     'cidade':'Santa Catarina'
     }
 ]
+
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
@@ -47,14 +49,16 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = self.argumentos.parse_args()
-        novo_hotel = {'hotel_id':hotel_id, **dados}
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
 
     def put(self, hotel_id):
         dados = self.argumentos.parse_args()
-        novo_hotel = {'hotel_id':hotel_id, **dados}
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = self.fing_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
